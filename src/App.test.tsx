2c1b80ca750416e13e9f5c40a1ebafe8d9d13c1a
Andika Eka Putra <me.andikaep@gmail.com>
2021-12-18 14:11:46 +0700

    test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 // import App from './App';
 import FormCalculator from './components/FormCalculator';
 import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
+const Wrapper = shallow(<FormCalculator />);
+
 // test('renders learn react link', () => {
 //   render(<App />);
 //   const linkElement = screen.getByText(/learn react/i);
@@ -16,8 +18,23 @@ describe('<App />', () => {
 	test('Halaman awal calculator dengan semua state null atau uncheck', async () => {});
 });
 
-test('Chek tombol bagi ', () => {
-  const button = shallow((<button name="bagi">/</button>));
-  button.find('button').simulate('click');
+test('Chek tombol bagi', () => {
+	const button = shallow(<button name='bagi'>/</button>);
+	button.find('button').simulate('click');
+});
 
+describe('Tampilan form kalkulator', () => {
+	it('test render formnya', () => {
+		render(<FormCalculator />);
+	});
+
+	it('Render Modul FormCalculator', () => {
+		expect(Wrapper).toMatchSnapshot();
+	});
+
+	describe('Mount component', () => {
+		describe('Event Test', () => {
+			it('Klik ceklist event', () => {});
+		});
+	});
 });
